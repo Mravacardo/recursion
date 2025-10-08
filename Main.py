5d96a5c1908df07c3532768d8563bed226f88d0b
Mravacardo <matthewheathcote@icloud.com>
2025-10-08 16:51:36 +0100

    Main.py

---
+++ Main.py
@@ -0,0 +1,30 @@
+# loop
+# sum = 0
+# for i in range(1, 11):
+#     print(i)
+#     sum += i
+#     print(sum)
+# recursive
+# def sumOfNumber(n):
+#     if n ==1 or n == 0:
+#         return n
+#     else:
+#         return n + sumOfNumber(n-1)
+# sumOfNumber(4)
+# print(sumOfNumber(4))
+# recursive fibonacci
+# def fibonacci(n):
+#     if n == 0 or n == 1:
+#         return n
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+# fibonacci(3)
+# print(fibonacci(3))
+# factorial
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# factorial(7)
+# print(factorial(7))
